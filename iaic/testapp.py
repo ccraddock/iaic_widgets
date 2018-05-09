2badbe6eaa3f208e070fd12bdf64510a11adb14c
swanandrakhade <35829870+swanandrakhade@users.noreply.github.com>
2018-05-09 10:35:56 -0500

    Add files via upload

--- iaic/testapp.py
+++ iaic/testapp.py
@@ -20,90 +20,6 @@ import plotly.figure_factory
 import pydicom
 
 
-studyID=""
-def createLog():
-  
-  images = []
-  images = glob.glob("./maskedimages/**/*.dcm", recursive=True)
-  # studynamefile=open("studyname.txt", "w")
-  
-  studyimage=images[0]
-  siread=pydicom.dcmread(studyimage)
-  global studyID
-  studyID=siread.StudyID
-  
-  ipfile = open("ip.txt", 'r')
-  ip = ipfile.readline().strip()
-  
-  try:
-    logs = pickle.load(open("logs.p", "rb"))
-    for i in range(len(images)):
-      now = datetime.datetime.now()
-      date = now.strftime("%Y-%m-%d")
-      
-      if (ip in logs):
-        
-        if (date in logs[ip]):
-          logs[ip][date].append(images[i])
-        else:
-          logs[ip][date] = []
-          logs[ip][date].append(images[i])
-      
-      
-      else:
-        
-        print("else")
-        logs[ip] = {}
-        logs[ip][date] = []
-        logs[ip][date].append(images[i])
-    
-    pickle.dump(logs, open("logs.p", "wb"))
-  
-  except:
-    logs = {}
-    logs[ip] = {}
-    now = datetime.datetime.now()
-    date = now.strftime("%Y-%m-%d")
-    
-    logs[ip][date] = []
-    
-    for i in range(len(images)):
-      logs[ip][date].append(images[i])
-    
-    pickle.dump(logs, open("logs.p", "wb"))
-
-
-def makecsv():
-  arrdata = transform()
-  
-  ips = []
-  dates = []
-  imagelists = []
-  studyNames=[]
-  for i in range(len(arrdata)):
-    ips.append(arrdata[i][0])
-    dates.append(arrdata[i][1])
-    imagelists.append(len(arrdata[i][2]))
-    studyNames.append(studyID)
-  # print(studyNames)
-  myitems = [("IP", ips), ("Dates", dates), ("Images", imagelists), ("Study ID", studyNames)]
-  
-  df = pd.DataFrame.from_items(myitems)
-  df.to_csv("logs.csv", index=False)
-
-
-def transform():
-  logs = pickle.load(open("logs.p", "rb"))
-  arrdata = []
-  for ip in logs:
-    for date in logs[ip]:
-      arr = []
-      arr.append(ip)
-      arr.append(date)
-      arr.append(logs[ip][date])
-      arrdata.append(arr)
-  
-  return arrdata
 
 
 app = dash.Dash()
@@ -187,6 +103,6 @@ app.css.append_css({
 })
 
 if __name__ == '__main__':
-  createLog()
-  makecsv()
+  # createLog()
+  # makecsv()
   app.run_server(debug=True, host="0.0.0.0")
